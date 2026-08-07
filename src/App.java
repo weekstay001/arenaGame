9addf2de65d4d0859765cf660e9f53af0f72d795
Taylor Weeks <weekstay001@warren.k12.in.us>
2026-08-07 12:35:59 -0400

    ArenaGame done

---
+++ src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String myName = "Weeks";
+        System.out.println(myName);
+
+        myName = "Taylor";
+        System.out.println(myName);
+
+        char myLetter = '\u00A9';
+        System.out.println(myLetter);
+
+        int x = 3;
+        short y = 3;
+        long z = 3L;
+        byte b = 3;
+
+        double d = 3.1415926538;
+        float f = 3.1415926538F;
+
+        boolean myBool = true;
+        boolean myBool2 = false;
+
+
+    }
+}
